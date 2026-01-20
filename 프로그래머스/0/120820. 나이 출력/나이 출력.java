8cf482532e19119d77c946d8a7a80d7686255758
bjw446 <bjw446@naver.com>
2026-01-20 10:34:31 +0900

    [level 0] Title: 나이 출력, Time: 10.80 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        answer = 2022 - age + 1;
+        System.out.println("2022년 기준 " + age + "살이므로 " + answer + "년생입니다.");
+        return answer;
+    }
+}
