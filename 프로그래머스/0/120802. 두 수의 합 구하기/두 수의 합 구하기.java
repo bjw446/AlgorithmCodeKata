9b4366436415722a8cbc3463d14bf6ca3836761c
bjw446 <bjw446@naver.com>
2026-01-21 09:20:33 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 20.92 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+            System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 " + num1 + " + " + num2 + " = " + answer + "를 return 합니다.");
+            return answer;
+    }
+}
